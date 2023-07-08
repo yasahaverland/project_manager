006a9c318625cc864aae773d8a4d6b4785bb92fb
yasahaverland <yasahaverland@icloud.com>
2023-07-08 18:44:22 -0300

    Graphgl installed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 const express = require('express')
-const port = process.env.PORT || 5000
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const port = process.env.PORT || 8000
 
 const app = express()
+
+app.use('graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on port ${port}`))
